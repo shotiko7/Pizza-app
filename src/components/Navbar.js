715c4da7aaa2f9c3693f7e1c3b648011bf93b075
Shota <shotikoavaliani97@gmail.com>
2022-07-17 17:05:00 +0400

    Add component folder and inside create navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div className='leftsidenav'> 
+
+        </div>
+        <div className='rightsidenav'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
