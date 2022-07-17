b73d7afff67618e1eb73c83a2b40533ec284f21e
Shota <shotikoavaliani97@gmail.com>
2022-07-17 23:06:16 +0400

    Navbar component css style and responsive is done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../assets/pizzaLogo.png"
 import "../styles/Navbar.css"
 import ReorderIcon from '@mui/icons-material/Reorder';
 import { Link } from "react-router-dom"
 
 function Navbar() {
+    const [hideLinks, setHideLinks] = useState(false)
+    
+    const onClickToogle =()=>{
+        setHideLinks(!hideLinks)       
+    }
   return (
     <div className='navbar'>
-        <div className='leftsidenav'> 
-            <img src={logo} alt="logo" />
+        <div className='leftsidenav' id={hideLinks ? 'open' : 'close'}> 
+           <img src={logo} alt="logo" />
+            <div className='hiddenLinks'>
+               <Link to="/"> Home </Link>
+               <Link to="/menu"> Menu </Link>
+               <Link to="/about"> About </Link>
+               <Link to="/contact"> Contact </Link>
+            </div>
         </div>
         <div className='rightsidenav'>
             <Link to="/"> Home </Link>
             <Link to="/menu"> Menu </Link>
             <Link to="/about"> About </Link>
             <Link to="/contact"> Contact </Link>
-            <button>
+            <button onClick={onClickToogle}>
                <ReorderIcon/>
             </button>
         </div>
