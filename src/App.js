bd8884ee13b5dc24a67b122a8056eee9cdf09f3a
Shota <shotikoavaliani97@gmail.com>
2022-07-19 12:21:26 +0400

    Create footer component and socialmedia icons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
