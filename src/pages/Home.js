b4be8c6c927bea5f1ec41c21ceac837805d7e39b
Shota <shotikoavaliani97@gmail.com>
2022-07-19 11:02:55 +0400

    Export Edu Tas font for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link }  from 'react-router-dom'
+import "../styles/Home.css"
+import Banner from "../assets/pizza-background.jpg"
 
 function  Home () {
   return (
-    <div> Hello Home  </div>
+    <div className='homecontainer' style={{backgroundImage: `url(${Banner})` }}> 
+      <div className='hometitle'>
+        <h1> Shota's Pizzeria </h1>
+        <p> You can choose anyone </p>
+        <Link to='/menu'>
+          <button> ORDER NOW </button>
+        </Link>
+      </div>
+      
+    </div>
   )
 }
 
